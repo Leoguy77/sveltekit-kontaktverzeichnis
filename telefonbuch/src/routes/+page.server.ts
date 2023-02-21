71887259b95e968535c8f0abdd813c2ea7314577
marvint24 <marvin.terme@gmail.com>
2023-02-21 14:01:33 +0100

    dataTable+pocketbase changes

--- telefonbuch/src/routes/+page.server.ts
+++ telefonbuch/src/routes/+page.server.ts
@@ -1,86 +1,119 @@
 import { error, redirect } from '@sveltejs/kit'
 import PocketBase from 'pocketbase';
 import dotenv from 'dotenv'
+import stringSimilarity from 'string-similarity'
 
 export const actions = {
 	search: async ({ request, locals }:any) => {
     const body = Object.fromEntries(await request.formData())
 
-    if (body.searchTxt){
-      let filter=body.searchTxt.split(" ").map((word:string)=>`index ?~ "${word}"`).join(" || ")
-
-      let pb
-      if (locals.pb.authStore.isValid){
-        pb=locals.pb
-      }else{
-        let env:any
-        env=dotenv.config({path: '../.env'})
-
-        pb=new PocketBase('http://127.0.0.1:8090')
-        await pb.collection('users').authWithPassword(env.parsed.APIUser, env.parsed.APIPW)
-      }
-      
-      let [persons, ressources] = await Promise.all([
-        pb.collection('person').getList(1, 20, {
-        filter: filter,
-        expand:"abteilungen,telefonEintraege,telefonEintraege.eintragTyp"}),
-        pb.collection('ressource').getList(1, 20, {
-          filter: filter,
-          expand:"abteilungen,telefonEintraege,telefonEintraege.eintragTyp"})
-      ])
-
-      function makeIterable(value: any): any{
-        if (typeof value[Symbol.iterator] === 'function'){
-          return value
-        }
-        return [value]
-      }
-
-      let result=[]
-
-      if(persons.items){
-        for (let person of makeIterable(persons.items)){
-          console.log(person)
-          let name=""
-          if(person.titel){name+=person.titel+" "}
-
-          let abteilungen=[]
-          console.log(person.expand)
-          // for (let abteilung of makeIterable(person.expand.abteilungen)) {
-          //   abteilungen.push(abteilung.bezeichnung)
-          // }
-          
-
-
-
-          let data={
-            id: person.id,
-            name: `${name}${person.vorname} ${person.nachname}`,
-            
-          }
-
-          console.log(data)
-        }
-      }
-
-      if(ressources.items){
-        for (let ressource of makeIterable(ressources.items)){
-          
-        }
-      }
-
-      if(result.length > 0){
-        return{data: structuredClone(persons)}
-      }
+    if (!body.searchTxt){
       return{nodata: true}
     }
 
-    // let persons = await locals.pb.collection('person').getList(1, 50, {
-    //   filter: 'created >= "2022-01-01 00:00:00" && someField1 != someField2',
-    //   ?expand=
-    // })
+    let filter=body.searchTxt.split(" ").map((word:string)=>`index ?~ "${word}"`).join(" || ")
 
-    // console.log(body)
-	},
+    let pb
+    if (locals.pb.authStore.isValid){
+      pb=locals.pb
+    }else{
+      let env:any
+      env=dotenv.config({path: '../.env'})
+
+      pb=new PocketBase('http://127.0.0.1:8090')
+      await pb.collection('users').authWithPassword(env.parsed.APIUser, env.parsed.APIPW)
+    }
+    
+    let [persons, ressources] = await Promise.all([
+      pb.collection('person').getList(1, 40, {
+      filter: filter,
+      expand:"abteilungen,telefonEintraege,telefonEintraege.eintragTyp"}),
+      pb.collection('ressource').getList(1, 40, {
+        filter: filter,
+        expand:"abteilungen,telefonEintraege,telefonEintraege.eintragTyp"})
+    ])
+
+    function makeIterable(value: any): any{
+      if (typeof value[Symbol.iterator] === 'function'){
+        return value
+      }
+      return [value]
+    }
+
+    let result=[]
+
+    if(persons.items){
+      for (let person of makeIterable(persons.items)){
+        let name=""
+        if(person.titel){name+=person.titel+" "}
+
+        let abteilungen=[]
+        if(person.expand.abteilungen){
+          for (let abteilung of makeIterable(person.expand.abteilungen)) {
+            abteilungen.push({id:abteilung.id,bezeichnung:abteilung.bezeichnung})
+          }
+        }
+
+        let telefonEintraege=[]
+        if(person.expand.telefonEintraege){
+          for (let telefonEintrag of makeIterable(person.expand.telefonEintraege)) {
+            telefonEintraege.push({id:telefonEintrag.id,
+              nummer:telefonEintrag.nummer,
+              eintragTyp:telefonEintrag.expand.eintragTyp.bezeichner
+            })
+          }
+        }
+
+        let similarity = stringSimilarity.compareTwoStrings(body.searchTxt, person.index)
+
+        let data={
+          similarity: similarity,
+          type: "person",
+          name: {name:`${name}${person.vorname} ${person.nachname}`,id: person.id},
+          abteilungen: abteilungen,
+          telefonEintraege: telefonEintraege
+        }
+        result.push(data)
+      }
+    }
+
+    if(ressources.items){
+      for (let ressource of makeIterable(ressources.items)){
+        let abteilungen=[]
+        if(ressource.expand.abteilungen){
+          for (let abteilung of makeIterable(ressource.expand.abteilungen)) {
+            abteilungen.push({id:abteilung.id,bezeichnung:abteilung.bezeichnung})
+          }
+        }
+
+        let telefonEintraege=[]
+        if(ressource.expand.telefonEintraege){
+          for (let telefonEintrag of makeIterable(ressource.expand.telefonEintraege)) {
+            telefonEintraege.push({id:telefonEintrag.id,
+              nummer:telefonEintrag.nummer,
+              eintragTyp:telefonEintrag.expand.eintragTyp.bezeichner
+            })
+          }
+        }
+
+        let similarity = stringSimilarity.compareTwoStrings(body.searchTxt, ressource.index)
+
+        let data={
+          similarity: similarity,
+          type: "ressource",
+          name: {name:ressource.bezeichner,id: ressource.id},
+          abteilungen: abteilungen,
+          telefonEintraege: telefonEintraege
+        }
+        result.push(data)
+      }
+    }
+
+    if(result.length > 0){
+      console.log(result)
+      return{data: structuredClone(result)}
+    }
+    return{nodata: true}
+  },
 
 };
--- telefonbuch/src/routes/+page.svelte
+++ telefonbuch/src/routes/+page.svelte
@@ -7,11 +7,11 @@
   export let form: any
   let loading = false
 
-  // $:if(form){
-  //   console.log("data da")
-  //   form.hi="joo"
-  //   console.log(form)
-  // }
+  $:if(form){
+    console.log(form.data)
+    // form.hi="joo"
+    // console.log(form)
+  }
 
   $:if(form?.data || form?.nodata){loading = false}
 
@@ -50,59 +50,48 @@
       {/if}
       {#if form?.data}
         <div class="resultTable">
-          <Link href="/">Allgemeine Suche</Link>
           <DataTable
             sortable
             headers={[
+              { key: "type", value: "" },
               { key: "name", value: "Name" },
-              { key: "Standort", value: "Standort", sort: false },
+              { key: "standort", value: "Standort", sort: false },
               { key: "Abteilung", value: "Abteilung", sort: false },
               { key: "Kontakt", value: "Kontakt", sort: false},
             ]}
-            rows={[
-              {
-                id: "a",
-                name: "Load Balancer 3",
-                protocol: "HTTP",
-                port: 3000,
-                rule: "Round robin",
-              },
-              {
-                id: "b",
-                name: "Load Balancer 1",
-                protocol: "HTTP",
-                rule: "Round robin",
-              },
-              {
-                id: "c",
-                name: "Load Balancer 2",
-                protocol: "HTTP",
-                port: 80,
-                rule: "DNS delegation",
-              },
-              {
-                id: "d",
-                name: "Load Balancer 6",
-                protocol: "HTTP",
-                port: 3000,
-                rule: "Round robin",
-              },
-              {
-                id: "e",
-                name: "Load Balancer 4",
-                protocol: "HTTP",
-                port: 443,
-                rule: "Round robin",
-              },
-              {
-                id: "f",
-                name: "Load Balancer 5",
-                protocol: "HTTP",
-                port: 80,
-                rule: "DNS delegation",
-              },
-            ]}
-          />
+            rows={form.data}
+            >
+            <svelte:fragment slot="cell" let:row let:cell>
+              {#if cell.key === "type" && cell.value === "person"}
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                </svg>
+              {:else if cell.key === "type" && cell.value === "ressource"}
+                <svg xmlns:xlink="http://www.w3.org/1999/xlink" fill="none" width="24" xmlns="http://www.w3.org/2000/svg" style="-webkit-print-color-adjust:exact" id="screenshot-3c675e20-7386-8038-8002-089dd1f6e7af" version="1.1" viewBox="190 518 24 24" height="24">
+                  <g id="shape-3c675e20-7386-8038-8002-089dd1f6e7af" style="fill:#000000" width="1em" height="1em" rx="0" ry="0">
+                    <g id="shape-3c675e20-7386-8038-8002-089dd1f6e7c3">
+                      <g class="fills" id="fills-3c675e20-7386-8038-8002-089dd1f6e7c3">
+                        <path rx="0" ry="0" style="fill:#f4f4f4;fill-opacity:1" d="M202.000,528.500L203.500,528.500L203.500,530.000L202.000,530.000ZM205.000,528.500L206.500,528.500L206.500,530.000L205.000,530.000ZM208.000,528.500L209.500,528.500L209.500,530.000L208.000,530.000ZM202.000,531.500L203.500,531.500L203.500,533.000L202.000,533.000ZM205.000,531.500L206.500,531.500L206.500,533.000L205.000,533.000ZM208.000,531.500L209.500,531.500L209.500,533.000L208.000,533.000ZM202.000,534.500L203.500,534.500L203.500,536.000L202.000,536.000ZM205.000,534.500L206.500,534.500L206.500,536.000L205.000,536.000ZM208.000,534.500L209.500,534.500L209.500,536.000L208.000,536.000ZM202.000,525.500L209.500,525.500L209.500,527.000L202.000,527.000Z">
+                        </path>
+                      </g>
+                    </g>
+                    <g id="shape-3c675e20-7386-8038-8002-089dd1f6e7c4">
+                      <g class="fills" id="fills-3c675e20-7386-8038-8002-089dd1f6e7c4">
+                        <path rx="0" ry="0" style="fill:#f4f4f4;fill-opacity:1" d="M211.000,522.500L200.500,522.500L200.500,521.750C200.499,520.922,199.828,520.251,199.000,520.250L196.000,520.250C195.172,520.251,194.501,520.922,194.500,521.750L194.500,522.500L193.000,522.500C192.172,522.501,191.501,523.172,191.500,524.000L191.500,537.500C191.501,538.328,192.172,538.999,193.000,539.000L211.000,539.000C211.828,538.999,212.499,538.328,212.500,537.500L212.500,524.000C212.499,523.172,211.828,522.501,211.000,522.500ZZM196.000,521.750L199.000,521.750L199.000,534.500L196.000,534.500ZM211.000,537.500L193.000,537.500L193.000,524.000L194.500,524.000L194.500,534.500C194.501,535.328,195.172,535.999,196.000,536.000L199.000,536.000C199.828,535.999,200.499,535.328,200.500,534.500L200.500,524.000L211.000,524.000Z">
+                        </path>
+                      </g>
+                    </g>
+                  </g>
+                </svg>
+              {:else if cell.key === "name"}
+                <Link href="/person/{cell.value.id}">{cell.value.name}</Link>
+              {:else if cell.key === "name"}
+                <Link href="/person/{cell.value.id}">{cell.value.name}</Link>
+              {/if}
+            </svelte:fragment>
+          
+          </DataTable>
         </div>
       {:else if form?.nodata}
         <h4>Keine Ergebisse gefunden</h4>
