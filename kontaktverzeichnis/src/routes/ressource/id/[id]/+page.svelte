0c685d1e8c75107e619aa5ce50717e1f552f3290
marvint24 <marvin.terme@gmail.com>
2023-03-13 14:50:45 +0100

    copy of person

---
+++ kontaktverzeichnis/src/routes/ressource/id/[id]/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Person from "$lib/components/person/Person.svelte";
+
+  export let form: any
+  export let data: any
+
+
+</script>
+<section>
+  {#if data?.user}
+    <svelte:component this={Person} bind:form={form} edit={true} bind:data={data}/>
+  {:else}
+    <svelte:component this={Person} bind:form={form} edit={false} bind:data={data}/>
+  {/if}
+</section>
+
+
+
+<style>
+  section{
+    margin: 2rem 3.5rem 0 3.5rem;
+  }
+</style>
