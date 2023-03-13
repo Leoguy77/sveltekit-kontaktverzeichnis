7747c090f2246c5095d580d6af4d36f21f25f293
marvint24 <marvin.terme@gmail.com>
2023-03-13 14:23:50 +0100

    added endpoint that redirects you to an empty person

---
+++ kontaktverzeichnis/src/routes/person/new/+server.ts
@@ -0,0 +1,31 @@
+import { error, redirect } from "@sveltejs/kit"
+
+export async function GET({ locals }: any) {
+  let person: any
+  let forward: boolean
+  try {
+    if (!locals?.pb?.authStore?.isValid) {
+      return new Response('{"message":"Not authenticated"}', {
+        status: 401,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+    }
+    let secureData = await locals.pb.collection("secureData").create()
+    person = await locals.pb
+      .collection("person")
+      .create({ secureData: secureData.id })
+    forward = true
+  } catch {
+    return new Response('{"message":"Internal Error"}', {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+  }
+  if (forward) {
+    throw redirect(303, `/person/id/${person.id}`)
+  }
+}
