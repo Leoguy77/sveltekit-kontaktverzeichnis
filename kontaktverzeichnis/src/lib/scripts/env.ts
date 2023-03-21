a1be802fc9096497021e92839731b24e77acf9cf
marvint24 <marvin.terme@gmail.com>
2023-03-21 15:20:49 +0100

    renamed env module

---
+++ kontaktverzeichnis/src/lib/scripts/env.ts
@@ -0,0 +1,8 @@
+import dotenv from "dotenv"
+import path from "path"
+
+export function getDotEnv() {
+  const __dirname = path.resolve()
+  let env = dotenv.config({ path: `${__dirname}/../.env` })
+  return env
+}
