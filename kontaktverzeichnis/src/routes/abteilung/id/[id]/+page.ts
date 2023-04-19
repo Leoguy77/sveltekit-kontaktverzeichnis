4a4f8ce6cc01f4036bb6ee57383c80eaf263b3e1
marvint24 <marvin.terme@gmail.com>
2023-04-19 16:33:34 +0200

    added department name

--- kontaktverzeichnis/src/routes/abteilung/id/[id]/+page.ts
+++ kontaktverzeichnis/src/routes/abteilung/id/[id]/+page.ts
@@ -1,6 +1,15 @@
 export const load = async ({ params, fetch }: any) => {
-  let entities = []
-  const response = await fetch(`/api/abteilung/${params.id}`)
-  entities = await response.json()
-  return { entities }
+  let [entities, department] = await Promise.all([
+    (async () => {
+      const response = await fetch(`/api/abteilung/${params.id}`)
+      return await response.json()
+    })(),
+    (async () => {
+      const response = await fetch("/api/abteilung")
+      let departments = await response.json()
+      return departments.find((department: any) => department.id === params.id)
+    })(),
+  ])
+
+  return { entities, department }
 }
