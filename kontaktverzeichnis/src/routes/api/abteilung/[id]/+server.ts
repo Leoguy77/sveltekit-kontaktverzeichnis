5393cb6054b6efed8d69791d5422f2af4a359cb3
marvint24 <marvin.terme@gmail.com>
2023-04-19 15:02:32 +0200

    abteilung entity load function

--- kontaktverzeichnis/src/routes/api/abteilung/[id]/+server.ts
+++ kontaktverzeichnis/src/routes/api/abteilung/[id]/+server.ts
@@ -1,65 +1,27 @@
 import dbCache from "$lib/scripts/dbCache.ts"
-
-dbCache.refreshCache()
+import { parseEntities } from "$lib/scripts/entityParser.ts"
 
 export async function GET({ params }: any) {
-  //try {
   let [persons, ressources] = dbCache.getCache()
-  let entries = [...persons, ...ressources]
+
   let abteilungenID = params.id
 
-  let filterResult = entries.filter((entry) => {
+  let filteredPersons = persons.filter((entry) => {
     if (entry.abteilungen.includes(abteilungenID)) return true
     else return false
   })
-  let Result: any = []
-  filterResult.forEach((entry: any) => {
-    let newEntry = entry
-    delete newEntry.abteilungen
-    delete newEntry.collectionId
-    delete newEntry.collectionName
-    delete newEntry.expand.abteilungen
-    delete newEntry.created
-    newEntry.expand.standort.forEach((standort: any) => {
-      delete standort.collectionId
-      delete standort.collectioName
-      delete standort.created
-      delete standort.updated
-      delete standort.id
-      delete standort.expand
-    })
-    newEntry.expand.telefonEintraege.forEach((telefonEintrag: any) => {
-      delete telefonEintrag.collectionId
-      delete telefonEintrag.collectionName
-      delete telefonEintrag.eintragTyp
-      delete telefonEintrag.created
-      delete telefonEintrag.updated
-      delete telefonEintrag.collectionId
-      delete telefonEintrag.collectioName
-      delete telefonEintrag.expand
-      delete telefonEintrag.id
-    })
-    delete newEntry.index
-    delete newEntry.secureData
-    delete newEntry.telefonEintraege
-    Result.push(newEntry)
+  let filteredRessources = ressources.filter((entry) => {
+    if (entry.abteilungen.includes(abteilungenID)) return true
+    else return false
   })
 
-  // console.log(filterResult)
+  let parsedEntities = parseEntities([filteredPersons, filteredRessources], null)
 
-  let res = JSON.stringify(Result)
+  let res = JSON.stringify(parsedEntities)
 
   return new Response(res, {
     headers: {
       "Content-Type": "application/json",
     },
   })
-  //   } catch (e) {
-  //     return new Response('{"message":"Internal Error"}' + e, {
-  //       status: 500,
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //     })
-  //   }
 }
