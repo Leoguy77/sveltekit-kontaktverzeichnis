20f8c8be8c50b681cb410c311aca2cde33bcaa4d
marvint24 <marvin.terme@gmail.com>
2023-04-20 12:58:26 +0200

    übersetzt

--- kontaktverzeichnis/src/routes/login/+page.svelte
+++ kontaktverzeichnis/src/routes/login/+page.svelte
@@ -11,21 +11,21 @@
 <div class="middle">
   {#if !data.user}
     <form action="?/login" method="POST" class="center" use:enhance>
-      <TextInput name="email" type="text" labelText="Username" placeholder="user@example.com" />
+      <TextInput name="email" type="text" labelText="Benutzername" placeholder="user@example.com" />
 
-      <PasswordInput name="password" labelText="Password" placeholder="mySecretPassword" />
+      <PasswordInput name="password" labelText="Passwort" placeholder="mySecretPassword" />
 
       <input type="hidden" name="previousPage" value={previousPage} />
 
-      <Button type="submit">Login</Button>
+      <Button type="submit">Anmelden</Button>
       {#if form?.error}
         <InlineNotification kind="error" title="Error" subtitle={form?.error} />
       {/if}
     </form>
   {:else}
-    <h3>You are logged in as {data.user.name}</h3>
+    <h3>Angemeldet als: {data.user.name}</h3>
     <form action="?/logout" method="POST" use:enhance>
-      <Button type="submit">Logout</Button>
+      <Button type="submit">Abmelden</Button>
     </form>
   {/if}
 </div>
