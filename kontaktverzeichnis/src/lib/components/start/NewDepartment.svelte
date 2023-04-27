af0fd52d0e5185f116b15dd84f1e64418574941a
Leoguy77 <leonschneider29@googlemail.com>
2023-04-27 13:06:47 +0200

    add margin below form and moved button right side

--- kontaktverzeichnis/src/lib/components/start/NewDepartment.svelte
+++ kontaktverzeichnis/src/lib/components/start/NewDepartment.svelte
@@ -2,13 +2,13 @@
   import Popup from "$lib/components/Popup.svelte"
   import { ComboBox, Button, Form, TextInput } from "carbon-components-svelte"
   import { enhance } from "$app/forms"
-  import { goto } from '$app/navigation';
+  import { goto } from "$app/navigation"
 
   export let form: any
   export let popup: string
 
-  let formData:any
-  let department:string
+  let formData: any
+  let department: string
   let standorte: any = []
   let standortId: string = ""
   let filterEintrag: any = (item: any, value: string) => {
@@ -16,25 +16,24 @@
   }
 
   async function submitForm() {
-    let result = await fetch ("/api/abteilung", {
+    let result = await fetch("/api/abteilung", {
       method: "POST",
       body: JSON.stringify({
         bezeichnung: department,
-      })
+      }),
     })
     const json = await result.json()
     await goto("/abteilung/id/" + json.id)
   }
 </script>
 
-<Popup bind:popup bind:form >
+<Popup bind:popup bind:form>
   <div class="Popup">
     <h4>Neue Abteilung</h4>
     <br />
     <form class="center" on:submit|preventDefault={submitForm}>
       <div class="TextInput">
         <TextInput
-          class="TextInput"
           labelText="Bezeichnung"
           helperText="Name der Abteilung"
           placeholder="Abteilungsname"
@@ -42,21 +41,22 @@
           size="xl"
           id="test"
           bind:value={department} />
-        
-          <Button type="submit">OK</Button>
-
       </div>
+      <div class="Button">
+        <Button type="submit">OK</Button>
+      </div>
+      
     </form>
   </div>
 </Popup>
 
 <style>
-  form {
+  .TextInput {
     margin-bottom: 1vh;
   }
-  .Button {
-    text-align: right;
-  }
+.Button{
+  text-align: right;
+}
   .Popup {
     width: 28vw;
   }
