4adb8f30e2cda2a0cacb760412b46c4c7f264061
marvint24 <marvin.terme@gmail.com>
2023-05-12 12:16:45 +0200

    splited dev and prod

--- kontaktverzeichnis/src/lib/scripts/db.ts
+++ kontaktverzeichnis/src/lib/scripts/db.ts
@@ -1,12 +1,13 @@
 import PocketBase from "pocketbase"
 import { building } from "$app/environment"
+import { APIPW, APIUser, SERVER } from "$env/static/private"
 
 let pb: PocketBase = new PocketBase("")
 
 if (!building) {
-  pb = new PocketBase("http://pocketbase:8090")
-  if (!process.env.APIUser || !process.env.APIPW) throw new Error("APIUser or APIPW not set")
-  await pb.collection("users").authWithPassword(process.env.APIUser, process.env.APIPW)
+  if (!APIUser || !APIPW) throw new Error("APIUser or APIPW not set")
+  pb = new PocketBase(`http://${SERVER}`)
+  await pb.collection("users").authWithPassword(APIUser, APIPW)
   pb.autoCancellation(false)
   console.log("DB loaded")
 }
