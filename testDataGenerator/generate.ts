1feb36b1fcc2aa79ac8c00ad7dbd7deef7e3a384
Leoguy77 <leonschneider29@googlemail.com>
2023-06-12 17:03:47 +0200

    Co-authored-by: Marvin Terme <marvint24@users.noreply.github.com>

--- testDataGenerator/generate.ts
+++ testDataGenerator/generate.ts
@@ -1,9 +1,8 @@
 import { faker } from "@faker-js/faker"
-import PocketBase from "pocketbase"
+import sql from "mssql"
+import type { config } from "mssql"
 import dotenv from "dotenv"
 import commandLineArgs from "command-line-args"
-import { setPersonIndex } from "../indexGenerator/generateIndex.js"
-import { setRessourceIndex } from "../indexGenerator/generateIndex.js"
 let env: any
 env = dotenv.config({ path: "../.env" })
 
@@ -14,6 +13,7 @@ const optionDefinitions = [
 
 const options = commandLineArgs(optionDefinitions)
 
+// Consts
 const departments = [
   "Personalabteilung",
   "Controlling",
@@ -29,190 +29,223 @@ const departments = [
   "Rechnungswesen",
   "Finanzbuchhaltung",
 ]
+const phonetyps = ["Festnetz", "Mobil", "Fax", "DECT"]
 
-const phonetyps = ["52dd7fli25k7ktd", "04rh0lpszfx3s49", "b45xv7hkvreka2s", "skh52iac3dw71xx"]
+// Database connect
+var config: config = {
+  server: process.env.DB_SERVER || "",
+  user: process.env.DB_USER || "",
+  password: process.env.DB_PW || "",
+  database: process.env.DB_Name || "",
+  port: Number(process.env.DB_Port) || 0,
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000,
+  },
+  options: {
+    encrypt: true,
+    trustServerCertificate: true,
+  },
+}
+const db = await sql.connect(config)
 
-const pb = new PocketBase("http://127.0.0.1:8090")
-pb.autoCancellation(false)
 
-await pb.admins.authWithPassword("admin@telefon.buch", "myFirstLogin")
 
-let departmentIds: any = []
-for (let departmentName of departments) {
-  let department: any = await pb.collection("abteilung").create({
-    bezeichnung: departmentName,
+async function initData() {
+  const table = new sql.Table('abteilung');
+  table.create = false; // presuming table already exists
+  table.columns.add('departmentName', sql.VarChar, { nullable: true });
+
+  for (let i=0;i<100;i++) {
+    table.rows.add(getRandomItem(departments))
+  }
+  
+  const request = new sql.Request(db);
+  request.bulk(table, (err, result) => {
+    console.log(err)
+    console.log("---------")
+    console.log(result)
   })
-  departmentIds.push(department.id)
+
+
+
+
+
+
+
+  // console.time("initData")
+  // // departments
+  // let departmentIds: any = []
+  // for (let i=0;i<1;i++) {
+  //   const request = new sql.Request(db)
+  //   request.input("departmentName", sql.VarChar, departments)
+
+  //   let department: any = await request.query(`INSERT INTO abteilung VALUES ?`, [departments])
+  //   departmentIds.push(department.recordset[0].id)
+  // }
+  // console.dir({deps:departmentIds})
+  // console.log({deps:departmentIds})
+  // console.log("hi")
+  // console.timeEnd("initData")
+
+
+  // phonetyps
+
+  // locations
 }
 
-const locations = [
-  ["Berlin", "0302-1132"],
-  ["Hamburg", "040-45561"],
-  ["München", "0501-7856"],
-  ["Köln", "06412-3498"],
-]
-
-const costunits = ["11111", "22222", "33333", "44444", "55555"]
-
-let locationIds: any[] = []
-
-for (let location of locations) {
-  let record: any = await pb.collection("standort").create({
-    bezeichnung: location[0],
-    vorwahl: location[1],
-  })
-  let data = { id: record.id, bezeichnung: location[0], vorwahl: location[1] }
-  locationIds.push(data)
-}
-
-function randomIntFromInterval(min: number, max: number) {
-  // min and max included
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
+initData()
 
 // return random item of array
 function getRandomItem<T>(array: T[]): T {
   return array[Math.floor(Math.random() * array.length)]
 }
 
-async function createRandomPhoneNummer() {
-  const standort = getRandomItem(locationIds)
+// const locations = [
+//   ["Berlin", "0302-1132"],
+//   ["Hamburg", "040-45561"],
+//   ["München", "0501-7856"],
+//   ["Köln", "06412-3498"],
+// ]
 
-  const phoneNumber = faker.phone.number(standort.vorwahl + "-" + "#".repeat(randomIntFromInterval(1, 5)))
+// const costunits = ["11111", "22222", "33333", "44444", "55555"]
 
-  const telefonEintrag: any = await pb.collection("telefonEintrag").create({
-    eintragTyp: getRandomItem(phonetyps),
-    nummer: phoneNumber,
-    standort: standort.id,
-  })
+// let locationIds: any[] = []
 
-  return telefonEintrag.id
-}
+// for (let location of locations) {
+//   let record: any = await pb.collection("standort").create({
+//     bezeichnung: location[0],
+//     vorwahl: location[1],
+//   })
+//   let data = { id: record.id, bezeichnung: location[0], vorwahl: location[1] }
+//   locationIds.push(data)
+// }
 
-async function createRandomSecureData() {
-  const secureData: any = await pb.collection("secureData").create({
-    personalNummer: faker.random.numeric(5),
-    kostenstelle: getRandomItem(costunits),
-  })
-  return secureData.id
-}
-
-async function getDepartment(): Promise<string> {
-  let department: string = getRandomItem(departmentIds)
-  return department
-}
-
-async function createRandomPerson() {
-  const firstName = faker.name.firstName()
-  const lastName = faker.name.lastName()
-  const email = `${firstName}.${lastName}@meinefirma.de`
-  const phoneNumber = await runRandomTimes(1, 6, createRandomPhoneNummer)
-  const title = faker.helpers.maybe<string>(() => faker.helpers.arrayElement(["Dr.", "Prof.", "Dr. med.", "Prof. Dr."]), {
-    probability: 0.1,
-  })
-  const abteilung = await runRandomTimes(1, 4, getDepartment)
-  const randomLocation = await getRandomItem(locationIds)
-  const location = randomLocation.id
-
-  const user = await pb.collection("person").create({
-    vorname: firstName,
-    nachname: lastName,
-    titel: title,
-    email: email,
-    telefonEintraege: phoneNumber,
-    abteilungen: abteilung,
-    standort: location,
-    secureData: await createRandomSecureData(),
-  })
-  return user
-}
-
-async function runRandomTimes(min: number, max: number, func: () => Promise<string>) {
-  let times = Math.floor(Math.random() * max + min)
-  let result: string[] = []
-  for (let i = 0; i < times; i++) {
-    let res: string = await func()
-    result.push(res)
-  }
-  return result
-}
-
-async function createRandomResource() {
-  const bezeichner = faker.random.word()
-  const abteilungen = await runRandomTimes(1, 4, getDepartment)
-  const standort = await getRandomItem(locationIds).id
-  const telefonEintraege = await runRandomTimes(1, 6, createRandomPhoneNummer)
-
-  const email = `${bezeichner}@meinefirma.de`
-
-  const resource = await pb.collection("ressource").create({
-    bezeichner: bezeichner,
-    abteilungen: abteilungen,
-    standort: standort,
-    telefonEintraege: telefonEintraege,
-    email: email,
-  })
-  return resource
-}
-
-function ifNotEmpty(value: string): string {
-  if (value) {
-    return value + " "
-  }
-  return ""
-}
-
-function makeIterable(value: any): any {
-  if (typeof value[Symbol.iterator] === "function") {
-    return value
-  }
-  return [value]
-}
+// function randomIntFromInterval(min: number, max: number) {
+//   // min and max included
+//   return Math.floor(Math.random() * (max - min + 1) + min)
+// }
 
 
-async function createPersonIndex(userid: string) {
-  const person = await pb.collection("person").getOne(userid, {
-    expand: "standort,abteilungen,telefonEintraege,telefonEintraege.eintragTyp",
-  })
-  // console.log(person)
 
-  setPersonIndex(person)
-}
+// async function createRandomPhoneNummer() {
+//   const standort = getRandomItem(locationIds)
 
-async function createResourceIndex(resourceid: string) {
-  const resource = await pb.collection("ressource").getOne(resourceid, {
-    expand: "standort,abteilungen,telefonEintraege,telefonEintraege.eintragTyp",
-  })
-  // console.log(resource)
+//   const phoneNumber = faker.phone.number(standort.vorwahl + "-" + "#".repeat(randomIntFromInterval(1, 5)))
 
-  setRessourceIndex(resource)
-}
+//   const telefonEintrag: any = await pb.collection("telefonEintrag").create({
+//     eintragTyp: getRandomItem(phonetyps),
+//     nummer: phoneNumber,
+//     standort: standort.id,
+//   })
 
-async function createEmptyResource() {
-  const resource = await pb.collection("ressource").create({})
-}
+//   return telefonEintrag.id
+// }
 
-async function createEmptyUser() {
-  const user = await pb.collection("person").create({
-    vorname: "Max",
-    nachname: "Mustermann",
-  })
-}
+// async function createRandomSecureData() {
+//   const secureData: any = await pb.collection("secureData").create({
+//     personalNummer: faker.random.numeric(5),
+//     kostenstelle: getRandomItem(costunits),
+//   })
+//   return secureData.id
+// }
 
-for (let index = 0; index < options.count; index++) {
-  let user = await createRandomPerson()
-  if (options.verbose) {
-    console.log(user)
-  }
-  createPersonIndex(user.id)
-}
-for (let index = 0; index < options.count; index++) {
-  let resource = await createRandomResource()
-  if (options.verbose) {
-    console.log(resource)
-  }
-  createResourceIndex(resource.id)
-}
-//createEmptyResource();
-//createEmptyUser();
-//createPersonIndex("pwyy0zju75trv3e");
+// async function getDepartment(): Promise<string> {
+//   let department: string = getRandomItem(departmentIds)
+//   return department
+// }
+
+// async function createRandomPerson() {
+//   const firstName = faker.name.firstName()
+//   const lastName = faker.name.lastName()
+//   const email = `${firstName}.${lastName}@meinefirma.de`
+//   const phoneNumber = await runRandomTimes(1, 6, createRandomPhoneNummer)
+//   const title = faker.helpers.maybe<string>(() => faker.helpers.arrayElement(["Dr.", "Prof.", "Dr. med.", "Prof. Dr."]), {
+//     probability: 0.1,
+//   })
+//   const abteilung = await runRandomTimes(1, 4, getDepartment)
+//   const randomLocation = await getRandomItem(locationIds)
+//   const location = randomLocation.id
+
+//   const user = await pb.collection("person").create({
+//     vorname: firstName,
+//     nachname: lastName,
+//     titel: title,
+//     email: email,
+//     telefonEintraege: phoneNumber,
+//     abteilungen: abteilung,
+//     standort: location,
+//     secureData: await createRandomSecureData(),
+//   })
+//   return user
+// }
+
+// async function runRandomTimes(min: number, max: number, func: () => Promise<string>) {
+//   let times = Math.floor(Math.random() * max + min)
+//   let result: string[] = []
+//   for (let i = 0; i < times; i++) {
+//     let res: string = await func()
+//     result.push(res)
+//   }
+//   return result
+// }
+
+// async function createRandomResource() {
+//   const bezeichner = faker.random.word()
+//   const abteilungen = await runRandomTimes(1, 4, getDepartment)
+//   const standort = await getRandomItem(locationIds).id
+//   const telefonEintraege = await runRandomTimes(1, 6, createRandomPhoneNummer)
+
+//   const email = `${bezeichner}@meinefirma.de`
+
+//   const resource = await pb.collection("ressource").create({
+//     bezeichner: bezeichner,
+//     abteilungen: abteilungen,
+//     standort: standort,
+//     telefonEintraege: telefonEintraege,
+//     email: email,
+//   })
+//   return resource
+// }
+
+// function ifNotEmpty(value: string): string {
+//   if (value) {
+//     return value + " "
+//   }
+//   return ""
+// }
+
+// function makeIterable(value: any): any {
+//   if (typeof value[Symbol.iterator] === "function") {
+//     return value
+//   }
+//   return [value]
+// }
+
+// async function createEmptyResource() {
+//   const resource = await pb.collection("ressource").create({})
+// }
+
+// async function createEmptyUser() {
+//   const user = await pb.collection("person").create({
+//     vorname: "Max",
+//     nachname: "Mustermann",
+//   })
+// }
+
+// for (let index = 0; index < options.count; index++) {
+//   let user = await createRandomPerson()
+//   if (options.verbose) {
+//     console.log(user)
+//   }
+// }
+// for (let index = 0; index < options.count; index++) {
+//   let resource = await createRandomResource()
+//   if (options.verbose) {
+//     console.log(resource)
+//   }
+// }
+// //createEmptyResource();
+// //createEmptyUser();
+// //createPersonIndex("pwyy0zju75trv3e");
