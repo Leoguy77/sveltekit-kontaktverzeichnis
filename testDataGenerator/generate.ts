3ee3db8a411c32874786890886ad2a46c249d586
Leoguy77 <leonschneider29@googlemail.com>
2023-06-13 16:00:57 +0200

    continued generate.ts
    Co-authored-by: Marvin Terme <marvint24@users.noreply.github.com>

--- testDataGenerator/generate.ts
+++ testDataGenerator/generate.ts
@@ -1,140 +1,211 @@
-// import { faker } from "@faker-js/faker"
-// import sql from "mssql"
-// import type { config } from "mssql"
-// import dotenv from "dotenv"
-// import commandLineArgs from "command-line-args"
-// let env: any
-// env = dotenv.config({ path: "../.env" })
+import { faker } from "@faker-js/faker"
+import sql from "mssql"
+import type { config } from "mssql"
+import dotenv from "dotenv"
+import commandLineArgs from "command-line-args"
+let env: any
+env = dotenv.config({ path: "../.env" })
 
-// const optionDefinitions = [
-//   { name: "verbose", alias: "v", type: Boolean },
-//   { name: "count", alias: "c", type: Number, defaultOption: true, defaultValue: 10 },
-// ]
+const optionDefinitions = [
+  { name: "verbose", alias: "v", type: Boolean },
+  { name: "count", alias: "c", type: Number, defaultOption: true, defaultValue: 10 },
+]
 
-// const options = commandLineArgs(optionDefinitions)
+const options = commandLineArgs(optionDefinitions)
 
-// // Consts
-// const departments = [
-//   ["Personalabteilung"],
-//   ["Controlling"],
-//   ["Vorstand"],
-//   ["Empfang"],
-//   ["Informationstechnologie"],
-//   ["Produktentwicklung"],
-//   ["Fuhrparkverwaltung"],
-//   ["Marketing"],
-//   ["Unternehmenskommunikation"],
-//   ["Vertrieb"],
-//   ["Kundenbetreuung"],
-//   ["Rechnungswesen"],
-//   ["Finanzbuchhaltung"],
-// ]
-// const phonetyps = [["Festnetz"], ["Mobil"], ["Fax"], ["DECT"]]
+// Consts
+const departments = [
+  ["Personalabteilung"],
+  ["Controlling"],
+  ["Vorstand"],
+  ["Empfang"],
+  ["Informationstechnologie"],
+  ["Produktentwicklung"],
+  ["Fuhrparkverwaltung"],
+  ["Marketing"],
+  ["Unternehmenskommunikation"],
+  ["Vertrieb"],
+  ["Kundenbetreuung"],
+  ["Rechnungswesen"],
+  ["Finanzbuchhaltung"],
+]
+const phonetyps = [["Festnetz"], ["Mobil"], ["Fax"], ["DECT"]]
 
-// const locations = [
-//   ["Berlin", "0302-1132"],
-//   ["Hamburg", "040-45561"],
-//   ["München", "0501-7856"],
-//   ["Köln", "06412-3498"],
-// ]
+const locations = [
+  ["Berlin", "0302-1132"],
+  ["Hamburg", "040-45561"],
+  ["München", "0501-7856"],
+  ["Köln", "06412-3498"],
+]
 
-async function main() {
-  console.profile()
-  console.log("start")
-  function sleep(ms: number) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, ms)
-    })
+const costunits = ["11111", "22222", "33333", "44444", "55555"]
+
+async function dbConnect() {
+  // Database connect
+  var config: config = {
+    server: process.env.DB_SERVER || "",
+    user: process.env.DB_USER || "",
+    password: process.env.DB_PW || "",
+    database: process.env.DB_Name || "",
+    port: Number(process.env.DB_Port) || 0,
+    pool: {
+      max: 10,
+      min: 0,
+      idleTimeoutMillis: 30000,
+    },
+    options: {
+      encrypt: true,
+      trustServerCertificate: true,
+    },
+  }
+  const db = await sql.connect(config)
+  return db
+}
+
+// Helper functions
+async function bulkInsert(tableName: string, columns: string[], values: string[][]) {
+  let table = new sql.Table(tableName)
+  table.create = false
+  for (let column of columns) {
+    table.columns.add(column, sql.VarChar, { nullable: true })
   }
 
-  await sleep(2000)
-  console.log("end")
-  console.profileEnd()
+  for (let row of values) {
+    table.rows.add(...row)
+  }
+
+  let request = new sql.Request(db)
+  let query = await request.query(`SELECT MAX(id) FROM ${tableName}`)
+  let minId = Number() + 1
+
+  request = new sql.Request(db)
+  let res = await request.bulk(table)
+
+  let ids = []
+  for (let i = minId; i < minId + res.rowsAffected; i++) {
+    ids.push(minId + i)
+  }
+
+  return ids
+}
+
+async function insertRow(tableName: string, columns: string[], values: string[]) {
+  if (columns.length != values.length) {
+    throw new Error("DB Insert: columns and values must have the same length")
+  }
+
+  let request = new sql.Request(db)
+
+  let colomnString = columns.join(",")
+  let valueString = columns.map((v) => "@" + v).join(",")
+  for (let i = 0; i < columns.length; i++) {
+    request.input(columns[i], sql.VarChar, values[i])
+  }
+  let id = await request.query(`INSERT INTO ${tableName} (${colomnString}) OUTPUT Inserted.ID VALUES (${valueString})`)
+  return id.recordset[0].ID
+}
+
+async function updateRow(tableName: string, columns: string[], id: number, newValues: string[]) {
+  if (columns.length != newValues.length) {
+    throw new Error("DB Insert: columns and values must have the same length")
+  }
+
+  let request = new sql.Request(db)
+
+  let setString = ""
+  for (let i = 0; i < columns.length; i++) {
+    request.input(columns[i], sql.VarChar, newValues[i])
+    setString += columns[i] + "=@" + columns[i] + ","
+  }
+  setString = setString.substring(0, setString.length - 1)
+
+  request.input("id", sql.Int, id)
+
+  await request.query(`UPDATE ${tableName} SET ${setString} WHERE id=@id`)
+}
+
+async function deleteRow(tableName: string, id: number) {
+  let request = new sql.Request(db)
+
+  request.input("val0", sql.Int, id)
+
+  await request.query(`DELETE FROM ${tableName} WHERE id=@val0`)
+}
+
+async function deleteJunction(tableName: string, columns: string[], id1: number, id2: number) {
+  if (columns.length != 2) {
+    throw new Error("DB Junction Delete: columns and values must have the same length")
+  }
+  let request = new sql.Request(db)
+
+  request.input("val0", sql.Int, id1)
+  request.input("val1", sql.Int, id2)
+
+  await request.query(`DELETE FROM ${tableName} WHERE ${columns[0]}=@val0 and ${columns[1]}=@val1`)
+}
+
+async function insertJunction(tableName: string, columns: string[], id1: number, id2: number) {
+  if (columns.length != 2) {
+    throw new Error("DB Junction Insert: columns and values must have the same length")
+  }
+  let request = new sql.Request(db)
+
+  request.input("val0", sql.Int, id1)
+  request.input("val1", sql.Int, id2)
+
+  await request.query(`INSERT INTO ${tableName} (${columns.join(",")}) VALUES (@val0, @val1)`)
+}
+
+// Tools
+function getRandomItem<T>(array: T[]): T {
+  return array[Math.floor(Math.random() * array.length)]
+}
+
+function randomIntFromInterval(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+async function initData() {
+  //  departments
+  await bulkInsert("abteilung", ["bezeichnung"], departments)
+  console.log("departments inserted")
+
+  // phonetyps
+  await bulkInsert("eintragTyp", ["bezeichnung"], phonetyps)
+  console.log("phonetyps inserted")
+
+  // locations
+  let ids = await bulkInsert("standort", ["bezeichnung", "vorwahl"], locations)
+  console.log(ids)
+  console.log("locations inserted")
+}
+
+async function createRandomPerson() {
+  const vorname = faker.name.firstName()
+  const nachname = faker.name.lastName()
+  const email = faker.internet.email(vorname, nachname)
+  // const standort = getRandomItem(locationIds)
+
+  // const personId = await insertRow("person", ["vorname", "nachname", "email", "standort"], [vorname, nachname, email, standort.id])
+
+  // return personId
+}
+
+// Main
+let db: sql.ConnectionPool
+let locationIds: number[] = []
+async function main() {
+  console.log("start")
+  db = await dbConnect()
+  await initData()
+
+  console.log("done")
+  await db.close()
 }
 
 main()
 
-// // Database connect
-// var config: config = {
-//   server: process.env.DB_SERVER || "",
-//   user: process.env.DB_USER || "",
-//   password: process.env.DB_PW || "",
-//   database: process.env.DB_Name || "",
-//   port: Number(process.env.DB_Port) || 0,
-//   pool: {
-//     max: 10,
-//     min: 0,
-//     idleTimeoutMillis: 30000,
-//   },
-//   options: {
-//     encrypt: true,
-//     trustServerCertificate: true,
-//   },
-// }
-// const db = await sql.connect(config)
-
-// async function bulkInsert(tableName: string, columns: string[], values: string[][]) {
-//   let table = new sql.Table(tableName)
-//   table.create = false
-//   for (let column of columns) {
-//     table.columns.add(column, sql.VarChar, { nullable: true })
-//   }
-
-//   for (let row of values) {
-//     table.rows.add(...row)
-//   }
-
-//   let request = new sql.Request(db)
-//   return await request.bulk(table)
-// }
-
-// async function initData() {
-//   // console.profile()
-//   //  departments
-//   await bulkInsert("abteilung", ["bezeichnung"], departments)
-//   console.log("departments inserted")
-
-//   // phonetyps
-//   await bulkInsert("eintragTyp", ["bezeichnung"], phonetyps)
-//   console.log("phonetyps inserted")
-
-//   // locations
-//   await bulkInsert("standort", ["bezeichnung", "vorwahl"], locations)
-//   console.log("locations inserted")
-//   // console.profileEnd()
-// }
-
-// await initData()
-
-// // return random item of array
-// function getRandomItem<T>(array: T[]): T {
-//   return array[Math.floor(Math.random() * array.length)]
-// }
-
-// const locations = [
-//   ["Berlin", "0302-1132"],
-//   ["Hamburg", "040-45561"],
-//   ["München", "0501-7856"],
-//   ["Köln", "06412-3498"],
-// ]
-
-// const costunits = ["11111", "22222", "33333", "44444", "55555"]
-
-// let locationIds: any[] = []
-
-// for (let location of locations) {
-//   let record: any = await pb.collection("standort").create({
-//     bezeichnung: location[0],
-//     vorwahl: location[1],
-//   })
-//   let data = { id: record.id, bezeichnung: location[0], vorwahl: location[1] }
-//   locationIds.push(data)
-// }
-
-// function randomIntFromInterval(min: number, max: number) {
-//   // min and max included
-//   return Math.floor(Math.random() * (max - min + 1) + min)
-// }
+// -------------------------
 
 // async function createRandomPhoneNummer() {
 //   const standort = getRandomItem(locationIds)
