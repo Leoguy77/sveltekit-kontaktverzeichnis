c75943206fb2ff178dfbdde5056cc5836bd6b80c
marvint24 <marvin.terme@gmail.com>
2023-06-14 14:30:01 +0200

    new db module

--- kontaktverzeichnis/src/lib/server/db.ts
+++ kontaktverzeichnis/src/lib/server/db.ts
@@ -1,15 +1,35 @@
-import PocketBase from "pocketbase"
-import { building } from "$app/environment"
-import { APIPW, APIUser, SERVER } from "$env/static/private"
+import sql from "mssql"
+import type { config } from "mssql"
+// import { building } from "$app/environment"
 
-let pb: PocketBase = new PocketBase("")
+console.log("process.env.NODE_ENV", process.env.NODE_ENV)
+console.log("hiii")
 
-if (!building) {
-  if (!APIUser || !APIPW) throw new Error("APIUser or APIPW not set")
-  pb = new PocketBase(`http://${SERVER}`)
-  await pb.collection("users").authWithPassword(APIUser, APIPW)
-  pb.autoCancellation(false)
-  console.log("DB loaded")
+if (process.env.NODE_ENV != "production") {
+  const { fileURLToPath } = await import("url")
+  const path = await import("path")
+  const __filename = fileURLToPath(import.meta.url)
+  const dotenv = await import("dotenv")
+  const envLocation = path.join(__filename, "./../../../../../.env")
+  dotenv.config({ path: envLocation })
+  console.log(envLocation)
 }
 
-export default pb
+var config: config = {
+  server: process.env.DB_SERVER || "",
+  user: process.env.DB_USER || "",
+  password: process.env.DB_PW || "",
+  database: process.env.DB_Name || "",
+  port: Number(process.env.DB_Port) || 0,
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000,
+  },
+  options: {
+    encrypt: true,
+    trustServerCertificate: true,
+  },
+}
+const db = await sql.connect(config)
+export default db
