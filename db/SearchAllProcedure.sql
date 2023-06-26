649cfe1ec78d2c4dcf2798a16813e004b4d04bb0
Leoguy77 <leonschneider29@googlemail.com>
2023-06-26 09:21:16 +0200

    created procedures

--- db/SearchAllProcedure.sql
+++ db/SearchAllProcedure.sql
@@ -27,36 +27,98 @@ GO
 
 
 ALTER PROCEDURE SearchAllPersons
-	@SearchTable SearchTableType READONLY
-	AS
-		-- SET NOCOUNT ON added to prevent extra result sets from
-		-- interfering with SELECT statements.
-		SET NOCOUNT ON;
-		SELECT * FROM Person 
-		JOIN standortperson on person.id = standortperson.personId
-		JOIN Standort on standort.id = standortperson.standortId
-		JOIN personabteilung on person.id = personabteilung.personId
-		JOIN abteilung on abteilung.id = personabteilung.abteilungId
-        -- Search for all persons containing any of the strings from the table
-        WHERE Person.vorname IN (SELECT String FROM @SearchTable) 
-		OR person.nachname IN (SELECT String FROM @SearchTable)
-		OR standort.bezeichnung IN (SELECT String FROM @SearchTable)
-		OR abteilung.bezeichnung IN (SELECT String FROM @SearchTable)
+@SearchTable SearchTableType READONLY
+AS
+SELECT
+    MAX(person.vorname) AS vorname,
+    MAX(person.nachname) AS nachname,
+    MAX(person.email) AS email,
+    person.id,
+    STUFF(
+        (SELECT DISTINCT ', ' + standort.bezeichnung
+         FROM Standort
+         JOIN standortperson ON standort.id = standortperson.standortId
+         WHERE standortperson.personId = person.id
+         FOR XML PATH(''), TYPE).value('.', 'nvarchar(MAX)'),
+        1, 2, '') AS standortbezeichnung,
+    STUFF(
+        (SELECT DISTINCT ', ' + abteilung.bezeichnung
+         FROM Abteilung
+         JOIN personabteilung ON abteilung.id = personabteilung.abteilungId
+         WHERE personabteilung.personId = person.id
+         FOR XML PATH(''), TYPE).value('.', 'nvarchar(MAX)'),
+        1, 2, '') AS abteilungbezeichnung,
+    STUFF(
+        (SELECT DISTINCT ', ' + telefonEintrag.nummer
+         FROM telefonEintrag
+         JOIN telefonEintragPerson ON telefonEintrag.id = telefonEintragPerson.telefonEintragId
+         WHERE telefonEintragPerson.personId = person.id
+         FOR XML PATH(''), TYPE).value('.', 'nvarchar(MAX)'),
+        1, 2, '') AS nummern,
+    (SELECT TOP 1 standort.vorwahl
+     FROM Standort
+     JOIN standortperson ON standort.id = standortperson.standortId
+     WHERE standortperson.personId = person.id) AS vorwahl,
+    STUFF(
+        (SELECT DISTINCT ', ' + eintragTyp.bezeichnung
+         FROM eintragTyp
+         JOIN telefonEintrag ON eintragTyp.id = telefonEintrag.eintragTypID
+         JOIN telefonEintragPerson ON telefonEintrag.id = telefonEintragPerson.telefonEintragId
+         WHERE telefonEintragPerson.personId = person.id
+         FOR XML PATH(''), TYPE).value('.', 'nvarchar(MAX)'),
+        1, 2, '') AS telefoneintragbezeichnung,
+    (SELECT TOP 1 telefonEintrag.eintragTypID
+     FROM telefonEintrag
+     JOIN telefonEintragPerson ON telefonEintrag.id = telefonEintragPerson.telefonEintragId
+     WHERE telefonEintragPerson.personId = person.id) AS eintragTypID
+FROM
+    Person
+    JOIN standortperson ON person.id = standortperson.personId
+    JOIN Standort ON standort.id = standortperson.standortId
+    JOIN personabteilung ON person.id = personabteilung.personId
+    JOIN abteilung ON abteilung.id = personabteilung.abteilungId
+    JOIN telefonEintragPerson ON person.id = telefonEintragPerson.personId
+    JOIN telefonEintrag ON telefonEintrag.id = telefonEintragPerson.telefonEintragId
+WHERE
+    Person.vorname IN (SELECT String FROM @SearchTable) 
+    OR person.nachname IN (SELECT String FROM @SearchTable)
+    OR standort.bezeichnung IN (SELECT String FROM @SearchTable)
+    OR abteilung.bezeichnung IN (SELECT String FROM @SearchTable)
+GROUP BY
+    person.id;
 GO
 
 ALTER PROCEDURE SearchAllRessources
+@SearchTable SearchTableType READONLY
+AS	
+SELECT 
+	ressource.id,
+	ressource.bezeichnung,
+	standort.bezeichnung as standortbezeichnung,
+	standort.vorwahl,
+	abteilung.bezeichnung as abteilungsbezeichnung,
+	telefonEintrag.nummer,
+	eintragTyp.bezeichnung as telefoneintragbezeichnung,
+	telefonEintrag.eintragTypID 
+FROM 
+	Ressource
+	JOIN standortressource on ressource.id = standortressource.ressourceId
+    JOIN Standort on standort.id = standortressource.standortId
+    JOIN ressourceabteilung on ressource.id = ressourceabteilung.ressourceId
+    JOIN abteilung on abteilung.id = ressourceabteilung.abteilungId
+	JOIN telefonEintragRessource on ressource.id = telefonEintragRessource.ressourceId
+	JOIN telefonEintrag on telefonEintrag.id = telefonEintragRessource.telefonEintragId
+	JOIN eintragTyp on telefonEintrag.eintragTypID = eintragTyp.id
+    -- Search for all abteilugen containing any of the strings from the table
+WHERE
+	Ressource.bezeichnung IN (SELECT String FROM @SearchTable)
+	OR standort.bezeichnung IN (SELECT String FROM @SearchTable)
+    OR abteilung.bezeichnung IN (SELECT String FROM @SearchTable)
+GO
+
+ALTER PROCEDURE SearchAll
 	@SearchTable SearchTableType READONLY
 	AS
-		-- SET NOCOUNT ON added to prevent extra result sets from
-		-- interfering with SELECT statements.
-		SET NOCOUNT ON;
-		SELECT * FROM Ressource
-        JOIN standortressource on ressource.id = standortressource.ressourceId
-        JOIN Standort on standort.id = standortressource.standortId
-        JOIN ressourceabteilung on ressource.id = ressourceabteilung.ressourceId
-        JOIN abteilung on abteilung.id = ressourceabteilung.abteilungId
-        -- Search for all abteilugen containing any of the strings from the table
-		WHERE Ressource.bezeichnung IN (SELECT String FROM @SearchTable)
-        OR standort.bezeichnung IN (SELECT String FROM @SearchTable)
-        OR abteilung.bezeichnung IN (SELECT String FROM @SearchTable)
+		EXEC [dbo].[SearchAllPersons] @SearchTable
+		EXEC [dbo].[SearchAllRessources] @SearchTable
 GO
