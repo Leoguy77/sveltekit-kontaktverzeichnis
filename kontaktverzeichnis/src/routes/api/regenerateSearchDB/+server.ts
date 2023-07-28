d19deab6f7f642c736145108ecbb2aca1c730cc3
marvint24 <marvin.terme@gmail.com>
2023-07-28 09:12:03 +0000

    generate search index

--- kontaktverzeichnis/src/routes/api/regenerateSearchDB/+server.ts
+++ kontaktverzeichnis/src/routes/api/regenerateSearchDB/+server.ts
@@ -20,9 +20,7 @@ export const GET: RequestHandler = async (event) => {
   (select eintragTyp.id, eintragTyp.bezeichnung from eintragTyp where eintragTyp.id=eintragTypId for json path, WITHOUT_ARRAY_WRAPPER) as eintragTyp
   from telefonEintrag 
   
-  join _personTotelefonEintrag on _personTotelefonEintrag.B=telefonEintrag.id
-  
-  where _personTotelefonEintrag.A=person.id for json path
+  where telefonEintrag.personId=person.id for json path
   
   ) as telefonEintrag,
   
@@ -45,7 +43,7 @@ export const GET: RequestHandler = async (event) => {
   from person 
   for json path) as result `
 
-  let personParse = JSON.parse(JSONPerson[0].result).map((obj: any) => {
+  let parsedPerson = JSON.parse(JSONPerson[0].result).map((obj: any) => {
     obj.id = `p_${obj.id}`
     obj.telefonEintrag = obj.telefonEintrag.map((telEintrag: any) => {
       telEintrag.standort = JSON.parse(telEintrag.standort)
@@ -55,33 +53,57 @@ export const GET: RequestHandler = async (event) => {
     return obj
   })
 
-  console.log(personParse.length)
-  await meili.index("entities").addDocuments(personParse)
+  let JSONRessource: any = await prisma.$queryRaw`
+    select (
+select ressource.id as id,
 
-  // if (JSONPerson) {
-  //   // console.log(JSON.stringify(personParse, null, 2))
-  // }
+  email,
+  bezeichnung,
 
-  // let [personen, ressourcen] = await Promise.all([
-  //   prisma.person.findFirst({
-  //     include: { standort: true, telefonEintrag: { include: { eintragTyp: true, standort: true } }, abteilung: true },
-  //   }),
-  //   prisma.ressource.findFirst({
-  //     include: { standort: true, telefonEintrag: { include: { eintragTyp: true, standort: true } }, abteilung: true },
-  //   }),
-  // ])
-  // // console.log(personen)
-  // // console.log(personen?.standort)
-  // console.log(ressourcen?.telefonEintrag)
-  // personen = personen.map((p: any) => {
-  //   p.id = `p_${p.id}`
-  //   return p
-  // })
-  // ressourcen = ressourcen.map((r: any) => {
-  //   r.id = `r_${r.id}`
-  //   return r
-  // })
-  // await meili.index("entities").addDocuments([...personen, ...ressourcen])
+  
+  (
+  select telefonEintrag.id,nummer,standortId,eintragTypId,
+  (select standort.id, standort.bezeichnung from standort
+   where standort.id=telefonEintrag.standortId
+  for json path, WITHOUT_ARRAY_WRAPPER) as  standort,
+  (select eintragTyp.id, eintragTyp.bezeichnung from eintragTyp where eintragTyp.id=eintragTypId for json path, WITHOUT_ARRAY_WRAPPER) as eintragTyp
+  from telefonEintrag 
+  
+  where telefonEintrag.ressourceId=ressource.id for json path
+  
+  ) as telefonEintrag,
+  
+  (select 
+  id,
+  bezeichnung
+  from _abteilungToressource
+  join abteilung on abteilung.id=_abteilungToressource.A
+  where _abteilungToressource.B=ressource.id for json path
+  ) as abteilung,
+  
+  (select 
+  id,
+  bezeichnung
+  from _ressourceTostandort
+  join standort on standort.id=_ressourceTostandort.B
+  where _ressourceTostandort.A=ressource.id for json path
+  ) as standort
+  
+  from ressource 
+  for json path
+  ) as result`
+
+  let parsedRessource = JSON.parse(JSONRessource[0].result).map((obj: any) => {
+    obj.id = `r_${obj.id}`
+    obj.telefonEintrag = obj.telefonEintrag.map((telEintrag: any) => {
+      telEintrag.standort = JSON.parse(telEintrag.standort)
+      telEintrag.eintragTyp = JSON.parse(telEintrag.eintragTyp)
+      return telEintrag
+    })
+    return obj
+  })
+
+  await meili.index("entities").addDocuments([...parsedPerson, ...parsedRessource])
 
   return new Response("done")
 }
