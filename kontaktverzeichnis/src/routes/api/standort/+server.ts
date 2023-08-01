97af4be9454e0ad37cb7d52fea9925c1abdd4a2d
marvint24 <marvin.terme@gmail.com>
2023-08-01 09:00:23 +0000

    apis for addNumber

---
+++ kontaktverzeichnis/src/routes/api/standort/+server.ts
@@ -0,0 +1,13 @@
+import prisma from "$lib/server/prisma.ts"
+import type { RequestHandler } from "@sveltejs/kit"
+
+export const GET: RequestHandler = async () => {
+  let eintragtypen = await prisma.standort.findMany()
+
+  return new Response(JSON.stringify(eintragtypen), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+}
