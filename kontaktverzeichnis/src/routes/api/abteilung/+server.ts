cc2044abc5a55b130300dbd025756e8fcbb716d0
Leoguy77 <LeonSchneider29@googlemail.com>
2023-08-04 10:57:07 +0200

    added api route to update department description

--- kontaktverzeichnis/src/routes/api/abteilung/+server.ts
+++ kontaktverzeichnis/src/routes/api/abteilung/+server.ts
@@ -4,11 +4,24 @@ export const GET: RequestHandler = async () => {
   const abteilungen = await prisma.abteilung.findMany({
     include: {
       _count: {
-        select: { person: true },
+        select: { person: true, ressource: true },
       },
     },
   })
   return new Response(JSON.stringify(abteilungen), {
+    headers: { "content-type": "application/json" },
+  })
+}
+export const PATCH: RequestHandler = async ({ request }) => {
+  const body = await request.json()
+  console.log(body)
+  const abteilung = await prisma.abteilung.update({
+    where: { id: body.id },
+    data: {
+      bezeichnung: body.bezeichnung,
+    },
+  })
+  return new Response(JSON.stringify(abteilung), {
     headers: { "content-type": "application/json" },
   })
 }
