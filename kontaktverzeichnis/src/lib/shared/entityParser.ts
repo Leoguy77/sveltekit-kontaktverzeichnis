9e346ece2b0d8169dd0bae76b329b9f2fb0fc8a7
marvint24 <marvin.terme@gmail.com>
2023-08-10 11:16:02 +0000

    fixed double id in searchTable

--- kontaktverzeichnis/src/lib/shared/entityParser.ts
+++ kontaktverzeichnis/src/lib/shared/entityParser.ts
@@ -2,6 +2,7 @@ export default function parseEntities(persons: any[], ressources: any[]) {
   if (persons) {
     persons = persons.map((v: any) => ({
       ...v,
+      id: `p_${v.id}`,
       name: { name: v.titel ? `${v?.titel + " "}${v.vorname} ${v.nachname}` : `${v.vorname} ${v.nachname}`, type: "person", id: v.id },
       kontakt: { telefonEintraege: v.telefonEintrag, email: v.email },
     }))
@@ -9,7 +10,8 @@ export default function parseEntities(persons: any[], ressources: any[]) {
   if (ressources) {
     ressources = ressources.map((v: any) => ({
       ...v,
-      name: { name: v.bezeichnung, type: "ressource" },
+      id: `r_${v.id}`,
+      name: { name: v.bezeichnung, type: "ressource", id: v.id },
       kontakt: { telefonEintraege: v.telefonEintrag, email: v.email, id: v.id },
     }))
   }
