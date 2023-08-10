b9ee2b6185d2c7db88a23aeb0fc6d7b3f0d60258
marvint24 <marvin.terme@gmail.com>
2023-08-10 11:44:43 +0000

    404 person, ressource
    Co-authored-by: Leon Schneider <Leoguy77@users.noreply.github.com>

--- kontaktverzeichnis/src/routes/person/[id]/+page.ts
+++ kontaktverzeichnis/src/routes/person/[id]/+page.ts
@@ -1,11 +1,16 @@
-// import { createCommonData } from "$lib/server/entityParser.ts"
 import type { PageLoad } from "./$types.ts"
+import { error } from "@sveltejs/kit"
 
 export const load: PageLoad = async ({ params, fetch }) => {
   if (params.id === "new") {
     return { person: { telefonEintrag: [], abteilung: [], standort: [] } }
   }
   let res = await fetch("/api/person/" + params.id)
+  if (res.status === 404) {
+    throw error(404, {
+      message: "Not found",
+    })
+  }
   let person = await res.json()
   return { person: person }
 }
--- kontaktverzeichnis/src/routes/ressource/[id]/+page.ts
+++ kontaktverzeichnis/src/routes/ressource/[id]/+page.ts
@@ -1,11 +1,16 @@
-// import { createCommonData } from "$lib/server/entityParser.ts"
 import type { PageLoad } from "./$types.ts"
+import { error } from "@sveltejs/kit"
 
 export const load: PageLoad = async ({ params, fetch }) => {
   if (params.id === "new") {
     return { ressource: { telefonEintrag: [], abteilung: [], standort: [] } }
   }
   let res = await fetch("/api/ressource/" + params.id)
+  if (res.status === 404) {
+    throw error(404, {
+      message: "Not found",
+    })
+  }
   let ressource = await res.json()
   return { ressource: ressource }
 }
