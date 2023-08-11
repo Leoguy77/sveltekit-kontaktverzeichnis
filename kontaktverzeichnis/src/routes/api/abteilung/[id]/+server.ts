8f508c34fe35f0f2888cf0dbbb731e54909e622e
Leoguy77 <LeonSchneider29@googlemail.com>
2023-08-11 11:04:39 +0200

    del kostenstelle+personalnummer when not logged in

--- kontaktverzeichnis/src/routes/api/abteilung/[id]/+server.ts
+++ kontaktverzeichnis/src/routes/api/abteilung/[id]/+server.ts
@@ -1,7 +1,7 @@
 import type { RequestHandler } from "./$types.ts"
 import { prismaInclude } from "$lib/shared/prismaTypes.ts"
 import prisma from "$lib/server/prisma.ts"
-export const GET: RequestHandler = async ({ params }) => {
+export const GET: RequestHandler = async ({ params, locals }) => {
   const abteilung = await prisma.abteilung.findUniqueOrThrow({
     where: { id: Number(params?.id) },
     include: {
@@ -13,6 +13,14 @@ export const GET: RequestHandler = async ({ params }) => {
       },
     },
   })
+  if (!locals.user) {
+    abteilung.person = abteilung.person.map((p) => {
+      p.kostenstelle = null
+      p.personalnummer = null
+      return p
+    })
+  }
+
   return new Response(JSON.stringify(abteilung), {
     headers: { "content-type": "application/json" },
   })
