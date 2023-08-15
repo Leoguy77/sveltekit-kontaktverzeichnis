db7e7537a47090239f95bfd5fe5ee3c1c06f74d0
marvint24 <marvin.terme@gmail.com>
2023-08-15 13:36:21 +0000

    env for dev

--- kontaktverzeichnis/src/routes/+page.ts
+++ kontaktverzeichnis/src/routes/+page.ts
@@ -2,14 +2,20 @@ import entityParser from "$lib/shared/entityParser.ts"
 import type { PageLoad } from "./$types.ts"
 import { PUBLIC_MEILI_SEARCH_KEY } from "$env/static/public"
 import { browser } from "$app/environment"
+import { PUBLIC_NODE_ENV } from "$env/static/public"
+
+let publicUrl =
+  PUBLIC_NODE_ENV === "dev"
+    ? "http://localhost:7700/indexes/entities/search/"
+    : "https://kontaktverzeichnis.gnh.net/indexes/entities/search/"
+
+let searchUrl = browser ? publicUrl : "http://meilisearch:7700/indexes/entities/search/"
 
 export const load: PageLoad = async (event) => {
+  console.log(searchUrl)
   let searchText = event.url.searchParams.get("q")
   if (searchText) {
     searchText = searchText.replace(/[/?=]|\s\s/g, "")
-    let searchUrl = browser
-      ? "https://kontaktverzeichnis.gnh.net/indexes/entities/search/"
-      : "http://meilisearch:7700/indexes/entities/search/"
     const response = await event.fetch(searchUrl, {
       method: "POST",
       body: JSON.stringify({ q: searchText, limit: 50 }),
